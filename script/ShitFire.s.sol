3177ddef982c3eb1b3192b8cbfc15a5ee67d620b
vboxuser <vboxuser@mylinx.myguest.virtualbox.org>
2024-09-10 10:23:20 +0700

    first commit

--- script/ShitFire.s.sol
+++ script/ShitFire.s.sol
@@ -2,12 +2,22 @@
 pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {ShitNFT} from "../src/ShitFire.sol";
+
 contract ShitNFTScript is Script {
-    ShitNFT public ShitNFT;
+    ShitNFT public shitNFTInstance; // Use the instance variable
     uint256 public deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
     function run() public {
         vm.startBroadcast(deployerPrivateKey);
-        ShitNFT = new ShitNFT();
+        
+        // Provide the required constructor arguments
+        address airdropToken = 0x38007e72a8826401E5d9a893217Be33E42aB5dA0; 
+        uint256 tokenPerNFT = 1000000000000000000; 
+        address rewardToken = 0x38007e72a8826401E5d9a893217Be33E42aB5dA0; 
+        uint256 rewardPerNFT = 1000000000000000000;
+        
+        shitNFTInstance = new ShitNFT(airdropToken, tokenPerNFT, rewardToken, rewardPerNFT);
+        
         vm.stopBroadcast();
     }
 }
